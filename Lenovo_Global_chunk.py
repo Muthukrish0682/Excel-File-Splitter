59d743c92b50b0ebf3c54127e3845c9b4f0cca9f
Muthukrish0682 <138483379+Muthukrish0682@users.noreply.github.com>
2023-07-06 18:24:25 +0530

    Add files via upload

---
+++ Lenovo_Global_chunk.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import streamlit as st
+import io
+
+def split_excel_file(input_file, chunk_size):
+    # Read the Excel file into a pandas DataFrame
+    df = pd.read_excel(input_file)
+
+    # Get the total number of rows in the DataFrame
+    total_rows = len(df)
+
+    # Calculate the number of chunks required
+    num_chunks = total_rows // chunk_size + (total_rows % chunk_size > 0)
+
+    # Split the DataFrame into chunks
+    chunks = [df[i*chunk_size:(i+1)*chunk_size] for i in range(num_chunks)]
+
+    # Save each chunk as a separate Excel file
+    output_files = []
+    for i, chunk in enumerate(chunks):
+        output_file = io.BytesIO()
+        chunk.to_excel(output_file, index=False)
+        output_files.append(output_file)
+
+    return output_files
+
+# Streamlit app
+st.title("👋 Excel File Splitter")
+
+# Chunk size input
+chunk_size = st.number_input("Enter the chunk size", min_value=1, step=1)
+
+# File upload
+file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+
+# Splitting and generating output files
+if chunk_size and file:
+    chunks = split_excel_file(file, chunk_size)
+    st.success("Excel file has been split into chunks!")
+
+    # Download buttons for each chunk file
+    st.subheader("Download Chunks")
+    for i, chunk in enumerate(chunks):
+        st.download_button(
+            label=f"Download Chunk {i+1}",
+            data=chunk.getvalue(),
+            file_name=f"output_chunk_{i+1}.xlsx"
+        )
